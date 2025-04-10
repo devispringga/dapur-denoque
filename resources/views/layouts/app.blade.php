fe07e9045c532353c556727b4e932c4032bf803f
DESKTOP-PH923VD\SMAN 20 GARUT <devispringga@gmail.com>
2025-04-11 00:01:00 +0700

    Created by git commit on 2023-10-01 12:34:56

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <title>@yield('title') - Dapur Denoque</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
 </head>
 <body>
 
